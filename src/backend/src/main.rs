17580be374ff3e0abafe139d9c612a64d6d8c0bf
Lattawit Kulanuwat <sususa003@gmail.com>
2025-06-21 08:02:09 +0200

    feat: add shared state for actix web

--- src/backend/src/main.rs
+++ src/backend/src/main.rs
@@ -1,11 +1,70 @@
 use actix_web::{App, HttpServer, middleware::DefaultHeaders};
+use ark_bn254::{Bn254, Fr};
+use ark_crypto_primitives::{
+    snark::SNARK,
+    sponge::poseidon::{PoseidonConfig, find_poseidon_ark_and_mds},
+};
+use ark_ff::PrimeField;
+use ark_ff::Zero;
+use ark_groth16::{Groth16, PreparedVerifyingKey, ProvingKey, prepare_verifying_key};
+use ark_std::rand::SeedableRng;
+use ark_std::rand::rngs::StdRng;
+
+use crate::zk::{
+    circuit::CIRCUIT_MAX_POLYGON_HASHES, circuit::CIRCUIT_MAX_VERTICES, circuit::CIRCUIT_PRECISION,
+    circuit::PointInMapCircuit, point_2d::Point2DDec,
+};
 
 mod api;
 mod state;
 mod zk;
 
+fn generate_point_in_map_keys<const PREC: u32, const MAX_VERTS: usize, const MAX_HASHES: usize>(
+    poseidon_cfg: PoseidonConfig<Fr>,
+) -> (ProvingKey<Bn254>, PreparedVerifyingKey<Bn254>) {
+    /* dummy circuit (all zeros) */
+    let zero_pt = Point2DDec::<Fr, PREC>::from_f64(0.0, 0.0);
+    let zero_poly = core::array::from_fn(|_| zero_pt);
+    let circuit = PointInMapCircuit::<Fr, PREC, MAX_VERTS, MAX_HASHES>::new(
+        zero_pt,
+        zero_poly,
+        0,
+        false,
+        [Fr::zero(); MAX_HASHES],
+        poseidon_cfg.clone(),
+    );
+
+    /* RNG compatible with arkworks (rand 0.8) */
+    let mut rng = StdRng::seed_from_u64(0u64);
+
+    let (pk, vk) =
+        Groth16::<Bn254>::circuit_specific_setup(circuit, &mut rng).expect("setup failed");
+    (pk, prepare_verifying_key(&vk))
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let (ark, mds) = find_poseidon_ark_and_mds::<Fr>(Fr::MODULUS_BIT_SIZE as u64, 3, 8, 31, 0);
+    let poseidon_config = PoseidonConfig {
+        full_rounds: 8,
+        partial_rounds: 31,
+        alpha: 17,
+        ark,
+        mds,
+        rate: 2,
+        capacity: 1,
+    };
+
+    let (pk, pvk) = generate_point_in_map_keys::<
+        CIRCUIT_PRECISION,
+        CIRCUIT_MAX_VERTICES,
+        CIRCUIT_MAX_POLYGON_HASHES,
+    >(poseidon_config.clone());
+
+    let shared = state::AppState::init(pk, pvk, poseidon_config).expect("init state");
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+
     println!("Starting server at 8080");
     HttpServer::new(move || {
         App::new()
--- src/backend/src/state.rs
+++ src/backend/src/state.rs
@@ -1,5 +1,30 @@
-pub struct AppState {}
+use std::{io::Result, sync::Arc};
+
+use actix_web::web::Data;
+use ark_bn254::{Bn254, Fr};
+use ark_crypto_primitives::sponge::poseidon::PoseidonConfig;
+use ark_groth16::{PreparedVerifyingKey, ProvingKey, VerifyingKey};
+
+pub struct AppState {
+    pub pk: ProvingKey<Bn254>,
+    pub pvk: PreparedVerifyingKey<Bn254>,
+    pub poseidon_config: PoseidonConfig<Fr>,
+    pub transformer_from: String,
+    pub transformer_to: String,
+}
 
 impl AppState {
-    pub fn init() {}
+    pub fn init(
+        pk: ProvingKey<Bn254>,
+        pvk: PreparedVerifyingKey<Bn254>,
+        poseidon_config: PoseidonConfig<Fr>,
+    ) -> Result<Data<Arc<Self>>> {
+        Ok(Data::new(Arc::new(Self {
+            pk,
+            pvk,
+            poseidon_config,
+            transformer_from: "EPSG:4326".to_string(),
+            transformer_to: "EPSG:3857".to_string(),
+        })))
+    }
 }
